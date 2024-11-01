5490dfec80321d199e24c08015063e7162d9dc0d
Clara Burgard <clara.burgard@gmail.com>
2024-11-01 16:19:51 +0100

    made some cleaning and have now a new beta version, to be discussed with Pierre

--- prepare_eORCA_grid_cdo.sh
+++ prepare_eORCA_grid_cdo.sh
@@ -4,6 +4,9 @@
 
 path1=/data/cburgard/PREPARE_FORCING/PREPARE_CAVITY_MASKS/raw/eORCA1.4.3_OpenSeas_OpenAllCav_ModStraights/
 
+####################################
+# Prepare a grid from the coordinates fine but I now have a python script doing this and adding lon_bnds and lat_bnds, so not needed I think, but I leave it here for the record
+
 #  There is an example of nco commands to create this file from coordinates.nc (input file used by NEMO for its grid description)
 # select glamt and gphit variables from the coordinates file
 ncks -O -C -a -v glamt,gphit $path1/coordinates.nc grid_eORCA1_T.nc
@@ -21,16 +24,11 @@ ncap2 -O -s 'dummy[y,x]=1b' grid_eORCA1_T.nc grid_eORCA1_T.nc
 ncatted -a coordinates,dummy,c,c,'glamt gphit' \
         -a units,glamt,c,c,'degrees_east'      \
         -a units,gphit,c,c,'degrees_north'  grid_eORCA1_T.nc
+####################################
 
 
-ncks -v nav_lat,nav_lon,bounds_nav_lon,bounds_nav_lat,deptht,deptht_bounds,e3t,time_counter /data/cburgard/CASIMIR_SIMU/raw/n42openc/n42openc_00910101_01001231_1Y_grid_T.nc NEMO_gridT_eORCA1_cdo.nc
-
-ncatted -a _CoordinateAxisType,dummy,d,, IMBIE_2km_stereo_withbnds.nc
-ncatted -a coordinates,dummy,c,c,lon lat IMBIE_2km_stereo_withbnds.nc
-ncatted -a bounds,dummy,d,, IMBIE_2km_stereo_withbnds.nc
-ncks -v dummy toto.nc IMBIE_2km_stereo_withbnds.nc
-ncrename -v lon,dummy toto.nc
-ncks -v lon IMBIE_2km_stereo_withbnds.nc toto.nc
+###### after creating the grids manually, I need to run the things below to make it work with CDO
+###### but to make it work with xesmf it seems unnecessary! ### also keeping this for archive here
 
 ncatted -O -a ,global,d,, IMBIE_2km_stereo_withbnds.nc IMBIE_2km_stereo_withbnds.nc
 ncap2 -O -s dummy[y,x]=1b IMBIE_2km_stereo_withbnds.nc IMBIE_2km_stereo_withbnds.nc
